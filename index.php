f16322a5cef3aa2d67d12183e0cd45240b9b0ad0
varunteja93 <varunteja93@users.noreply.github.com>
2016-04-03 20:01:14 -0400

    Added files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>Login</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <form action="login-script.php" method="post">
+   <div id="mydiv">
+   
+   <h1 class="ht">Dashboard Login</h1>
+   <div>
+        <h4 class="ht">Username</h4>
+        <input style="color: #fff;" type="text" name="username" size="30">
+        </div>
+    <div>
+    <h4 class="ht">Password</h4>
+    <input style="color: #fff;" type="password" name="password" size="30">
+        </div>
+    <div>
+        <br>
+        <input type="submit" name="Login" value="Log In">
+    </div>
+    </div>
+    </form>
+</body>
+</html>

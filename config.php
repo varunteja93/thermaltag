ae0ab177f8067c73bbd595c5a1a0959fa97e3d7c
varunteja93 <varunteja93@users.noreply.github.com>
2016-04-23 11:57:19 -0400

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $host = "localhost";
-    $user = "root"; // type username here
-    $password = ""; //set password here
-    $database_name = "simple_login";
-    $pdo = new PDO("mysql:host=$host;dbname=$database_name", $user, $password, array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ));
-?>
